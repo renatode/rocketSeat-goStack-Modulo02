a838c0c018e01667d307260f0cf5935b3913aa13
renatode <renato.esnarriaga@gmail.com>
2019-08-26 22:55:34 -0300

    modulo 2

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd82dca775b49601dd3a772d337745f78',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Dcba4321!',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
